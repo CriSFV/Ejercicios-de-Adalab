e2422ab49c705449f18c347c6bcbb07313d9f0d8
CriSFV <cristina.f.valverde@gmail.com>
2021-09-25 11:08:41 +0200

    ex1 modulo-03-lesson-06-ejercicio-01

---
+++ modulo-03/modulo-03-leccion-06-ejercicio-01/src/components/App.js
@@ -0,0 +1,15 @@
+import '../styles/App.scss';
+import Header from './header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main className='principal'>
+        <h1 className='principal__text'>Creando diversidad digital</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
